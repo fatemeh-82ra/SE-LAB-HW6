f472336f8ef64c63013a9035c4a29b80a7369585
Haniyeh Sadat Miremadi <114092435+Haniyem@users.noreply.github.com>
2025-08-30 01:26:36 +0330

    Update SymbolTable.java

--- src/main/java/MiniJava/semantic/symbol/SymbolTable.java
+++ src/main/java/MiniJava/semantic/symbol/SymbolTable.java
@@ -36,7 +36,7 @@ public class SymbolTable {
     }
 
     public void addField(String fieldName, String className) {
-        klasses.get(className).Fields.put(fieldName, new Symbol(lastType, mem.getDateAddress()));
+        klasses.get(className).Fields.put(fieldName, new Symbol(lastType, mem.allocateDateAddress()));
     }
 
     public void addMethod(String className, String methodName, int address) {
@@ -55,7 +55,7 @@ public class SymbolTable {
         if (klasses.get(className).Methodes.get(methodName).localVariable.containsKey(localVariableName)) {
             ErrorHandler.printError("This variable already defined");
         }
-        klasses.get(className).Methodes.get(methodName).localVariable.put(localVariableName, new Symbol(lastType, mem.getDateAddress()));
+        klasses.get(className).Methodes.get(methodName).localVariable.put(localVariableName, new Symbol(lastType, mem.allocateDateAddress()));
 //        }catch (NullPointerException e){
 //            e.printStackTrace();
 //        }
@@ -155,8 +155,8 @@ public class SymbolTable {
             this.codeAddress = codeAddress;
             this.returnType = returnType;
             this.orderdParameters = new ArrayList<>();
-            this.returnAddress = mem.getDateAddress();
-            this.callerAddress = mem.getDateAddress();
+            this.returnAddress = mem.allocateDateAddress();
+            this.callerAddress = mem.allocateDateAddress();
             this.parameters = new HashMap<>();
             this.localVariable = new HashMap<>();
         }
@@ -168,7 +168,7 @@ public class SymbolTable {
         }
 
         public void addParameter(String parameterName) {
-            parameters.put(parameterName, new Symbol(lastType, mem.getDateAddress()));
+            parameters.put(parameterName, new Symbol(lastType, mem.allocateDateAddress()));
             orderdParameters.add(parameterName);
         }
 
