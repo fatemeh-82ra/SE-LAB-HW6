9df5ebeaf9dd1b0fef137342e564b941688811b4
Haniyeh Sadat Miremadi <114092435+Haniyem@users.noreply.github.com>
2025-08-29 11:36:33 +0330

    Update StandardTariff.java

--- src/main/java/com/energy/management/strategy/StandardTariff.java
+++ src/main/java/com/energy/management/strategy/StandardTariff.java
@@ -1,4 +1,5 @@
 package com.energy.management.strategy;
 public class StandardTariff implements CostCalculationStrategy {
-
+    @Override
+    public double calculateCost(int energyUnits) { return energyUnits * 500.0; }
 }
